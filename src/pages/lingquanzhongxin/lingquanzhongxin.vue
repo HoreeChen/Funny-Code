774e15e080369409244618940536b544913f9a68
LAPTOP-VD71SCPS\34698 <wangbiye001@163.com>
2021-12-03 17:40:19 +0800

    小尝试

--- src/pages/lingquanzhongxin/lingquanzhongxin.vue
+++ src/pages/lingquanzhongxin/lingquanzhongxin.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		领券中心
+		<view class="top">
+			wangbiye
+		</view>
+		<view class="top2">
+			wangbiye1
+		</view>
 	</view>
 </template>
 
@@ -17,6 +22,26 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.top{
+		height: 60rpx;
+		width: 650rpx;
+		background-color: #0081FF;
+		justify-content: center;
+		text-align: center;
+		color: #FFFFFF;
+		line-height: 60rpx;
+		margin: 0rpx 50rpx;
+	}
+	.top2{
+		height: 60rpx;
+		width: 650rpx;
+		background-color: #ffff00;
+		justify-content: center;
+		text-align: center;
+		color: #FFFFFF;
+		line-height: 60rpx;
+		margin:  50rpx;
+	}
 
 </style>
